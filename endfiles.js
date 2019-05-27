5e00f985af44847b08bdb9af8a377ce638299eb3
Egor <egor@opensrc.club>
2019-05-27 03:28:52 +0300

    Add ability to download from server

---
+++ endfiles.js
@@ -0,0 +1,178 @@
+endfilesCallback(
+{
+	"ja/th06": {
+		"source": "https://www16.big.or.jp/~zun/html/th06.html",
+		"encoding": "ms932",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end",
+			"staff00.end"
+		]
+	},
+	"ja/th07": {
+		"source": "https://www16.big.or.jp/~zun/html/th07.html",
+		"encoding": "ms932",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end",
+			"end20b.end",
+			"end20.end",
+			"end21.end",
+			"staff00.end"
+		]
+	},
+	"ja/th08": {
+		"source": "https://www16.big.or.jp/~zun/html/th08top.html",
+		"encoding": "ms932",
+		"files": [
+			"end00a.end",
+			"end00b.end",
+			"end00c.end",
+			"end01a.end",
+			"end01b.end",
+			"end01c.end",
+			"end02a.end",
+			"end02b.end",
+			"end02c.end",
+			"end03a.end",
+			"end03b.end",
+			"end03c.end",
+			"staff00b.end",
+			"staff00.end"
+		]
+	},
+	"ja/th09": {
+		"source": "https://www16.big.or.jp/~zun/html/th09top.html",
+		"encoding": "ms932",
+		"files": [
+			"end00.end",
+			"end01.end",
+			"end02.end",
+			"end03.end",
+			"end04.end",
+			"end05.end",
+			"end06.end",
+			"end07.end",
+			"end08.end",
+			"end09.end",
+			"end10.end",
+			"end11.end",
+			"end12.end",
+			"end13.end",
+			"endstaff.end"
+		]
+	},
+	"en/th06": {
+		"source": "https://en.touhouwiki.net/wiki/The_Embodiment_of_Scarlet_Devil/English_patch",
+		"encoding": "windows-1252",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end"
+		]
+	},
+	"en/th07": {
+		"source": "https://en.touhouwiki.net/wiki/Perfect_Cherry_Blossom/English_patch",
+		"encoding": "windows-1252",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end",
+			"end20b.end",
+			"end20.end",
+			"end21.end"
+		]
+	},
+	"en/th08": {
+		"source": "http://radicalr.pestermom.com/game.html#IN",
+		"encoding": "windows-1252",
+		"files": [
+			"end00a.end",
+			"end00b.end",
+			"end00c.end",
+			"end01a.end",
+			"end01b.end",
+			"end01c.end",
+			"end02a.end",
+			"end02b.end",
+			"end02c.end",
+			"end03a.end",
+			"end03b.end",
+			"end03c.end"
+		]
+	},
+	"en/th09": {
+		"source": "https://www.shrinemaiden.org/forum/index.php?topic=582.0",
+		"encoding": "windows-1252",
+		"files": [
+			"end00.end",
+			"end01.end",
+			"end02.end",
+			"end03.end",
+			"end04.end",
+			"end05.end",
+			"end06.end",
+			"end07.end",
+			"end08.end",
+			"end09.end",
+			"end10.end",
+			"end11.end",
+			"end12.end",
+			"end13.end"
+		]
+	},
+	"ru/th06": {
+		"source": "https://web.archive.org/web/20160516123740/http://yukarin.su/yukarin/games/touhou-06/",
+		"encoding": "windows-1251",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end"
+		]
+	},
+	"es/th06": {
+		"source": "http://moriya-shrine.blogspot.com/2015/09/Touhou06-Parche-v1.1-Final.html",
+		"encoding": "windows-1252",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end"
+		]
+	},
+	"es/th07": {
+		"source": "http://bunbunmaru.freeforums.org/touhou-7-11-y-12-parches-en-espanol-t1434.html",
+		"encoding": "windows-1252",
+		"files": [
+			"end00b.end",
+			"end00.end",
+			"end01.end",
+			"end10b.end",
+			"end10.end",
+			"end11.end",
+			"end20b.end",
+			"end20.end",
+			"end21.end"
+		]
+	}
+}
+);
